0520989938d7231a53c6a7cb80a39a735708a734
Joilr <colamanx@protonmail.com>
2023-11-25 09:54:02 +0100

    added functions

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-
-//Function myGrid
-//To "grid-container" it adds "<div class="grid-row"></div>" 16 times
-//To every "grid-row" it adds "<div class="grid-item"></div>" 16 times
-
-function myGrid() {
+// Function to create a 16x16 grid
+function createGrid() {
     const gridContainer = document.querySelector('.grid-container');
-
 
     for (let i = 0; i < 16; i++) {
         const gridRow = document.createElement('div');
         gridRow.className = 'grid-row';
-        gridContainer.appendChild(gridRow);
-
         for (let x = 0; x < 16; x++) {
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
             gridRow.appendChild(gridItem);
         }
-
+        gridContainer.appendChild(gridRow);
     } 
 }
 
-document.addEventListener("DOMContentLoaded", myGrid);
+// Function to change the color of a grid item
+function changeColor(target) {
+    target.style.backgroundColor = '#ff0000'; // Change to your desired color
+}
+
+// Event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid();
+
+    const gridContainer = document.querySelector('.grid-container');
+    let isMouseDown = false;
+
+    gridContainer.addEventListener('mousedown', (event) => {
+        if (event.target.classList.contains('grid-item')) {
+            isMouseDown = true;
+            changeColor(event.target);
+        }
+    });
+
+    gridContainer.addEventListener('mouseover', (event) => {
+        if (isMouseDown && event.target.classList.contains('grid-item')) {
+            changeColor(event.target);
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        isMouseDown = false;
+    });
+});
