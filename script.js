d5e1a9422affc1f7bf6e2d889aee14d5f52e9c13
Joilr <colamanx@protonmail.com>
2023-11-25 19:45:28 +0100

    added function to change grid size

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // Global variable to hold the current selected color
-let currentColor = '#ff0000';
+let currentColor = '#16DFB7';
+// Global variable to hold the current amount of pixels
+let currentPixels = 16;
 
 // Function to create a 16x16 grid
 function createGrid() {
     const gridContainer = document.querySelector('.grid-container');
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < currentPixels; i++) {
         const gridRow = document.createElement('div');
         gridRow.className = 'grid-row';
-        for (let x = 0; x < 16; x++) {
+        for (let x = 0; x < currentPixels; x++) {
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
             gridRow.appendChild(gridItem);
         }
         gridContainer.appendChild(gridRow);
     } 
+}
+
+//Function to change the amount of pixel in grid
+function updateSliderValue(value) {
+    document.getElementById('sliderValue').textContent = value;
+    currentPixels = value;
+
+    // delete current grid
+    const gridRows = document.querySelectorAll('.grid-row');
+    gridRows.forEach(row => {
+        row.remove();
+    });
+
+    //create new
+    createGrid();
 }
 
 // Function to change the color of a grid item
@@ -25,6 +42,20 @@ function changeColor(target) {
 // Function to update the current color
 function updateColor(newColor) {
     currentColor = newColor;
+    document.documentElement.style.setProperty('--hover-color', currentColor);
+}
+
+// Function to wipe
+function wipeGrid() {
+    currentColor = '#f0f0f0';
+}
+
+// Function to reset all grid items to the standard color
+function resetGrid() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) => {
+        item.style.backgroundColor = '#f0f0f0';
+    });
 }
 
 // Event listener for DOMContentLoaded
@@ -56,9 +87,16 @@ document.addEventListener("DOMContentLoaded", () => {
         isMouseDown = false;
     });
 
-    // Event listeners for color buttons
-    document.querySelector('#redColor').addEventListener('click', () => updateColor('#ff0000'));
-    document.querySelector('#blueColor').addEventListener('click', () => updateColor('#0000ff'));
-    document.querySelector('#yellowColor').addEventListener('click', () => updateColor('#ffff00'));
+     // Event listener for the color picker input
+     const colorPicker = document.querySelector('#colorPicker');
+     colorPicker.addEventListener('input', (event) => {
+         updateColor(event.target.value);
+     });
+
+        // Event listener for the wipe button
+        document.querySelector('#wipeButton').addEventListener('click', wipeGrid);
+
+        // Event listener for the reset button
+        document.querySelector('#resetButton').addEventListener('click', resetGrid);
     
 });
